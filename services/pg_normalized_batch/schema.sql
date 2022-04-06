1cd2673d232ad50b9020e394fe19b087e4e6bd71
Salih Erdal <erdsal.4@gmail.com>
2022-04-05 20:47:16 -0700

    finish assignment

--- services/pg_normalized_batch/schema.sql
+++ services/pg_normalized_batch/schema.sql
@@ -31,7 +31,7 @@ CREATE TABLE users (
  * Tweets may be entered in hydrated or unhydrated form.
  */
 CREATE TABLE tweets (
-    id_tweets BIGINT PRIMARY KEY,
+    id_tweets BIGINT,
     id_users BIGINT,
     created_at TIMESTAMPTZ,
     in_reply_to_status_id BIGINT,
@@ -60,25 +60,19 @@ CREATE INDEX tweets_index_withheldincountries ON tweets USING gin(withheld_in_co
 
 CREATE TABLE tweet_urls (
     id_tweets BIGINT,
-    url TEXT,
-    PRIMARY KEY (id_tweets, url),
-    FOREIGN KEY (id_tweets) REFERENCES tweets(id_tweets) DEFERRABLE INITIALLY DEFERRED
+    url TEXT
 );
 
 
 CREATE TABLE tweet_mentions (
     id_tweets BIGINT,
-    id_users BIGINT,
-    PRIMARY KEY (id_tweets, id_users),
-    FOREIGN KEY (id_tweets) REFERENCES tweets(id_tweets) DEFERRABLE INITIALLY DEFERRED
+    id_users BIGINT
 );
 CREATE INDEX tweet_mentions_index ON tweet_mentions(id_users);
 
 CREATE TABLE tweet_tags (
     id_tweets BIGINT,
-    tag TEXT,
-    PRIMARY KEY (id_tweets, tag),
-    FOREIGN KEY (id_tweets) REFERENCES tweets(id_tweets) DEFERRABLE INITIALLY DEFERRED
+    tag TEXT
 );
 COMMENT ON TABLE tweet_tags IS 'This table links both hashtags and cashtags';
 CREATE INDEX tweet_tags_index ON tweet_tags(id_tweets);
@@ -87,9 +81,7 @@ CREATE INDEX tweet_tags_index ON tweet_tags(id_tweets);
 CREATE TABLE tweet_media (
     id_tweets BIGINT,
     url TEXT,
-    type TEXT,
-    PRIMARY KEY (id_tweets, url),
-    FOREIGN KEY (id_tweets) REFERENCES tweets(id_tweets) DEFERRABLE INITIALLY DEFERRED
+    type TEXT
 );
 
 /*
